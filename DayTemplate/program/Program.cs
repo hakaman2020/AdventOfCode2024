173876655e4f1273c798519907ff62b1c95708d7
hakaman2020 <73297566+hakaman2020@users.noreply.github.com>
2024-12-01 07:18:39 +0100

    Template committed

---
+++ DayTemplate/program/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+string inputFilePath = "./example.txt";
+//string inputFilePath = "./input.txt";
+
+List<string> fileLines = ReadFileLines(inputFilePath);
+
+foreach(var line in fileLines){
+    Console.WriteLine(line);
+}
+
+List<string> ReadFileLines(string inputFile){
+    return File.ReadLines(inputFile).ToList();
+}
