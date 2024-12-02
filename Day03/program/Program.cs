4f6d03237121512a07f658bc2a2e6cd6df270584
hakaman2020 <73297566+hakaman2020@users.noreply.github.com>
2024-12-02 13:27:32 +0100

    prep day03

---
+++ Day03/program/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string inputFilePath = "./example.txt";
+//string inputFilePath = "./input.txt";
+
+List<string> fileLines = ReadFileLines(inputFilePath);
+
+Console.WriteLine($"Result of Task 1 is {Task1()}");
+Console.WriteLine($"Result of Task 2 is {Task2()}");
+
+int Task1(){
+    return 0;
+}
+
+int Task2(){
+    return 0;
+}
+
+List<string> ReadFileLines(string inputFile){
+    return File.ReadLines(inputFile).ToList();
+}
