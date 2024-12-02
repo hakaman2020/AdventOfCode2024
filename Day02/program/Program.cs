29a28f5e84db0b18d3b3295c51378f33965d0494
hakaman2020 <73297566+hakaman2020@users.noreply.github.com>
2024-12-02 13:16:36 +0100

    Final version of AOC day2

--- Day02/program/Program.cs
+++ Day02/program/Program.cs
@@ -1,16 +1,10 @@
-﻿
-using System.Runtime;
-using System.Runtime.InteropServices.Marshalling;
-using System.Security.Cryptography;
-using System.Transactions;
-
-//string inputFilePath = "./example.txt";
+﻿// string inputFilePath = "./example.txt";
 string inputFilePath = "./input.txt";
 
 List<string> fileLines = ReadFileLines(inputFilePath);
 
 Console.WriteLine($"Result of Task 1 is {Task1(fileLines)}");
-Console.WriteLine($"Result of Task 2 is {Task2()}");
+Console.WriteLine($"Result of Task 2 is {Task2(fileLines)}");
 
 int Task1(List<string> fileLines){
 
@@ -18,19 +12,19 @@ int Task1(List<string> fileLines){
 
     foreach(string line in fileLines){
         List<int> levels = line.Split(' ').Select(x => Convert.ToInt32(x)).ToList();
-        if(isItSafe(levels, false)){
+        if(isItSafe(levels)){
             amountSafeReports++;
         }
     }
     return amountSafeReports;
 }
 
-int Task2(){
+int Task2(List<string> fileLines){
     int amountSafeReports = 0;
 
     foreach(string line in fileLines){
         List<int> levels = line.Split(' ').Select(x => Convert.ToInt32(x)).ToList();
-        if(isItSafe(levels,true)){
+        if(isItSafeFaultTolerant(levels)){
             amountSafeReports++;
         }
     }
@@ -41,48 +35,28 @@ List<string> ReadFileLines(string inputFile){
     return File.ReadLines(inputFile).ToList();
 }
 
-Boolean isItSafe(List<int> report, bool allowedFaulty) {
+Boolean isItSafe(List<int> report) {
     
     int prevLevel = -1;
-    int levelMotion = reportAscDesc(report, allowedFaulty);
-    int faultyCount = 0;
-    
-    if (levelMotion == 0) {
-        return false;
-    }
+    int levelMotion = 0;
 
     for(int i = 0; i < report.Count; i++){
         
         int currentLevel = report[i];
-                
         if(i == 0){
             prevLevel = currentLevel;
+            levelMotion = currentLevel > report[1] ? -1 : 1; 
             continue;
         }
 
         if(currentLevel == prevLevel){
-            if(allowedFaulty && faultyCount == 0){
-                faultyCount = 1;
-                continue;
-            }
             return false;
         }
         
         int differenceLevel = currentLevel - prevLevel;
         int currentMotion = differenceLevel < 0 ? -1 : 1;
         
-        if(currentMotion != levelMotion){
-            if(allowedFaulty && faultyCount == 0){
-                faultyCount = 1;
-                continue;
-            }
-            return false;
-        }
-        if(Math.Abs(differenceLevel) > 3){
-            if(allowedFaulty && faultyCount == 0){
-                faultyCount = 1;
-                continue;
-            }
+        if(currentMotion != levelMotion || Math.Abs(differenceLevel) > 3){
             return false;
         }
         prevLevel = currentLevel;
@@ -90,33 +64,18 @@ Boolean isItSafe(List<int> report, bool allowedFaulty) {
     return true;
 }
 
-int reportAscDesc(List<int> report, bool allowedFaulty){
-    int prevLevel = 0;
-    int countAsc = 0;
-    int countDesc = 0;
-
-    for(int i = 0 ; i < report.Count; i++){
-        int currentLevel = report[i];
-        if(i == 0){
-            prevLevel = currentLevel;
-            continue;
-        }    
-        if( currentLevel < prevLevel){
-            countDesc++;
-        }
-        else if (currentLevel > prevLevel){
-            countAsc++;
-        }
-        else return 0;
-    }
-
+Boolean isItSafeFaultTolerant(List<int> report) {
     
-
-    if(countAsc == 0 && countDesc > 1
-        || countAsc == 1  && countDesc > 1 && allowedFaulty)
-        return -1;
-    if(countDesc == 0 && countAsc > 1
-        || countDesc == 1  && countAsc > 1 && allowedFaulty)
-        return 1;
-    return 0; //return 0 is an false
+    if(isItSafe(report)){
+        return true;
+    }
+    for(int i = 0; i < report.Count; i++){
+        List<int> tempList = new List<int>(report);
+        tempList.RemoveAt(i);
+        if(isItSafe(tempList)){
+            return true;
+        }
+    }
+    return false;
 }
+
