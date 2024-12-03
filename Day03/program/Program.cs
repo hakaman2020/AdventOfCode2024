71efd74937329d78873d8b2239cfba78da476366
hakaman2020 <73297566+hakaman2020@users.noreply.github.com>
2024-12-03 11:26:23 +0100

    Added alternative way to do task 2 after discovering other solution with REGEX

--- Day03/program/Program.cs
+++ Day03/program/Program.cs
@@ -10,6 +10,7 @@ string combinedString = CombineStrings(fileLines);
 
 Console.WriteLine($"Result of Task 1 is {Task1(combinedString)}");
 Console.WriteLine($"Result of Task 2 is {Task2(combinedString)}");
+Console.WriteLine($"Result of Task 2 calculated with a expanded regex pattern is {Task2ExpandedRegex(combinedString)}");
 
 int Task1(string line){
     return CalculateSumFromLine(line);
@@ -17,8 +18,14 @@ int Task1(string line){
 
 int Task2(string line){
     string processedLine = PreprocessLine(line);
+    Console.WriteLine(CalculateSumFromLineWithToggle(processedLine));
     return CalculateSumFromLine(processedLine);    
 }
+
+int Task2ExpandedRegex(string line){
+    return CalculateSumFromLineWithToggle(line);
+}
+
 
 string CombineStrings(List<string> fileLines){
     StringBuilder sb = new StringBuilder();
@@ -81,4 +88,33 @@ string PreprocessLine(string line){
 
 List<string> ReadFileLines(string inputFile){
     return File.ReadLines(inputFile).ToList();
+}
+
+//this function uses an expanded regex pattern so you don't have to do the line processing in 
+//to handle the don't and do. 
+int CalculateSumFromLineWithToggle(string line){
+    int sum = 0;
+    string pattern = @"don't\(\)|mul\(\d{1,3},\d{1,3}\)|do\(\)";
+    bool enabled = true;
+
+    foreach(Match match in Regex.Matches(line,pattern)){
+        if(match.Value.Equals("don't()")){
+            enabled = false;
+        }
+        else if(match.Value.Equals("do()")){
+            enabled = true;
+        }
+        else if (enabled){
+            string digitspattern = @"\d{1,3}";
+            List<int> numbers = Regex.Matches(match.ToString(), digitspattern).Select(i => Convert.ToInt32(i.ToString())).ToList();
+            sum += numbers[0] * numbers[1];
+        }
+
+        //alternative
+        //int num1 = int.Parse(match.Groups[1].Value);
+        //int num2 = int.Parse(match.Groups[2].Value);
+        //sum += num1 * num2;
+    }
+    
+    return sum;
 }
