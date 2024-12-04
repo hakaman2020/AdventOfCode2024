3a4ba0951fd34711085a4a374a4f3f59124abb66
hakaman2020 <73297566+hakaman2020@users.noreply.github.com>
2024-12-04 07:25:37 +0100

    WIP part 2

--- Day04/program/Program.cs
+++ Day04/program/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.CompilerServices;
 
 // string inputFilePath = "./example.txt";
@@ -7,7 +8,7 @@ string inputFilePath = "./input.txt";
 List<string> fileLines = ReadFileLines(inputFilePath);
 
 Console.WriteLine($"Result of Task 1 is {Task1(fileLines)}");
-Console.WriteLine($"Result of Task 2 is {Task2()}");
+Console.WriteLine($"Result of Task 2 is {Task2(fileLines)}");
 
 int Task1(List<string> puzzle){
 
@@ -25,7 +26,17 @@ int Task1(List<string> puzzle){
     return totalcount;
 }
 
-int Task2(){
+int Task2(List<string> puzzle){
+     int height = puzzle.Count;
+    int width = puzzle[0].Length;
+    int totalcount = 0
+
+     for(int row = 0; row < height; row++){
+        for(int column = 0; column < width; column++){
+            if(puzzle[row][column] == 'M')
+                totalcount += countXDashMAS(row, column, height, width, puzzle);    
+        }
+    }
     return 0;
 }
 
@@ -94,4 +105,8 @@ int countXmas(int row, int column, int height, int width, List<string> puzzle){
     }
     
     return count;
+}
+
+int countXDashMAS(int row, int column, int width, int height, List<string> puzzle){
+    return 0;
 }
