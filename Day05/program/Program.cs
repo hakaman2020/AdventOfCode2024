00a263f05f5ed19a86722a0823dd61acd99b47c2
hakaman2020 <73297566+hakaman2020@users.noreply.github.com>
2024-12-07 12:45:55 +0100

    Day07 finished and Day05 WIP

--- Day05/program/Program.cs
+++ Day05/program/Program.cs
@@ -1,8 +1,6 @@
 ﻿
-using System.Security;
-
 string inputFilePath = "./example.txt";
-//string inputFilePath = "./input.txt";
+// string inputFilePath = "./input.txt";
 
 List<string> fileLines = ReadFileLines(inputFilePath);
 
@@ -11,31 +9,97 @@ List<List<int>> updates = new();
 
 SeperateSections(fileLines, rules, updates);
 
+// //print the rules
+Console.WriteLine("\nRules");
 foreach(var set in rules){
     Console.Write(set.Key +  "|");
     var test= set.Value.ToList();
     test.ForEach(x => Console.Write(x + " "));
-    Console.WriteLine("");
+    Console.WriteLine();
 }
 
+// //print the updates
+// Console.WriteLine("\nUpdates");
+// foreach(List<int> update in updates){
+//     update.ForEach(x => Console.Write(x + ","));
+//     Console.WriteLine();
+// }
 
-Console.WriteLine($"Result of Task 1 is {Task1()}");
-Console.WriteLine($"Result of Task 2 is {Task2()}");
+(int sumTask1, List<List<int>> wrongUpdates) = Task1(rules,updates);
 
-int Task1(){
+Console.WriteLine($"Result of Task 1 is {sumTask1}");
+Console.WriteLine($"Result of Task 2 is {Task2(rules, wrongUpdates)}");
+
+(int,List<List<int>>) Task1(Dictionary<int,HashSet<int>> rules, List<List<int>> updates){
+
+    int sum = 0;
+    //keep track of wrong updates for task 2
+    List<List<int>> wrongUpdates = new();
+
+    foreach(var update in updates){
+        if(IsUpdateCorrect(rules, update)){
+            int middleIndex = update.Count / 2;
+            sum += update[middleIndex];
+        }
+        else{
+            wrongUpdates.Add(update);
+        }
+    }
+    return (sum, wrongUpdates);
+}
+
+int Task2(Dictionary<int, HashSet<int>> rules, List<List<int>> updates){
+
+    Console.WriteLine("Wrong Updates");
+    foreach(List<int> update in updates){
+        update.ForEach(x => Console.Write(x + ","));
+        Console.WriteLine();
+    }
     return 0;
 }
 
-int Task2(){
-    return 0;
+List<int> TopologicalSortUpdate(Dictionary<int, HashSet<int>> rules, List<int> update){
+    //will use Topological Sorting to sort
+    //start with all the numbers without dependencies
+    //add them to the queue
+    //remove those numbers from the numbers that are the dependant on numbers that where added to the queue
+    //repeat until no numbers are left
+
+    
+    //count dependencies
+    while(update.Count > 0){
+        
+    }
 }
+
+
+bool IsUpdateCorrect(Dictionary<int,HashSet<int>> rules, List<int> update){
+
+    if(update.Count == 1) return true;
+
+    for(int i = 1; i < update.Count; i++){
+        int checkingNumber = update[i];
+        if(rules.ContainsKey(checkingNumber)){
+            for(int j = 0; j < i; j++){
+                if(rules[checkingNumber].Contains(update[j])){
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
 
 List<string> ReadFileLines(string inputFile){
     return File.ReadLines(inputFile).ToList();
 }
 
 void SeperateSections(List<string> inputLines, Dictionary<int,HashSet<int>> rules, List<List<int>> updates){
+
     bool endrules = false;
+
     foreach (string line in inputLines){
         if(line == string.Empty){
             endrules = true;
---
+++ Day07/program/Program.cs
@@ -0,0 +1,79 @@
+﻿
+// string inputFilePath = "./example.txt";
+string inputFilePath = "./input.txt";
+
+List<string> fileLines = ReadFileLines(inputFilePath);
+
+Console.WriteLine($"Result of Task 1 is {Task1(fileLines)}");
+Console.WriteLine($"Result of Task 2 is {Task2(fileLines)}");
+
+long Task1(List<string> fileLines){
+
+    long sum = 0;
+
+    foreach(string line in fileLines){
+        var split = line.Split(' ');
+        long target = long.Parse(split[0].Substring(0, split[0].Length-1));
+        List<long> numbers = split.Skip(1).Select(n => long.Parse(n)).ToList();
+        if(IsEquationPossible(target, numbers, false)){
+            sum += target;
+        }
+    }
+
+    return sum;
+}
+
+long Task2(List<string> fileLines){
+
+    long sum = 0;
+
+    foreach(string line in fileLines){
+        var split = line.Split(' ');
+        long target = long.Parse(split[0].Substring(0, split[0].Length-1));
+        List<long> numbers = split.Skip(1).Select(n => long.Parse(n)).ToList();
+        if(IsEquationPossible(target, numbers, true)){
+            sum += target;
+        }
+    }
+
+    return sum;
+}
+
+List<string> ReadFileLines(string inputFile){
+    return File.ReadLines(inputFile).ToList();
+}
+
+bool IsEquationPossible(long target, List<long> numbers, bool concatOperatorActive){
+
+    long result = numbers[0];
+
+    if(BuildEquation(target, numbers, 1, result, Operator.Add, concatOperatorActive) 
+        || BuildEquation(target, numbers, 1, result, Operator.Multiply, concatOperatorActive)
+        || (BuildEquation(target, numbers, 1, result, Operator.Concat, true) && concatOperatorActive))
+        return true;
+    
+    return false;
+}
+
+bool BuildEquation(long target, List<long> numbers, int index, long result, Operator op, bool concatOperatorActive){
+    if(op == Operator.Add)
+        result += numbers[index];
+    else if(op == Operator.Multiply)
+        result *= numbers[index];
+    else if(concatOperatorActive && op == Operator.Concat)
+        result = long.Parse(result.ToString() + numbers[index].ToString());
+    if(index == numbers.Count - 1)
+        return result == target ? true : false;
+    if(BuildEquation(target, numbers, index + 1, result, Operator.Add, concatOperatorActive)
+        || BuildEquation(target, numbers, index + 1, result, Operator.Multiply, concatOperatorActive)
+        || (concatOperatorActive && BuildEquation(target, numbers, index + 1, result, Operator.Concat, concatOperatorActive)))
+        return true;
+
+    return false;
+}
+
+enum Operator{
+    Add,
+    Multiply,
+    Concat
+}
