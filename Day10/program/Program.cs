9930ab0a33caba3985371d3202486d803e4f04ae
hakaman2020 <73297566+hakaman2020@users.noreply.github.com>
2024-12-10 09:24:28 +0100

    Cleaned up Day10 code

--- Day10/program/Program.cs
+++ Day10/program/Program.cs
@@ -6,48 +6,51 @@ List<string> fileLines = ReadFileLines(inputFilePath);
 
 List<List<int>> map = ConvertToIntMap(fileLines);
 
-// foreach(List<int> line in map){
-//     line.ForEach(n => Console.Write(n));
-//     Console.WriteLine();
-// }
-Console.WriteLine($"Result of Task 1 is {Task1(map)}");
-Console.WriteLine($"Result of Task 2 is {Task2()}");
+(int uniqueReachedNineHeightPositions, int sumRating) results = CalculateResults(map);
+Console.WriteLine($"Result of Task 1 is {results.uniqueReachedNineHeightPositions}");
+Console.WriteLine($"Result of Task 2 is {results.sumRating}");
 
-int Task1(List<List<int>> map){
+(int,int) CalculateResults(List<List<int>> map){
+
     int amountReachableNineHeightPositions = 0;
+    int sumRating = 0;
     for(int y = 0; y < map.Count; y++){
         for(int x = 0; x < map[0].Count; x++){
-            if(map[y][x] == 0)
-                amountReachableNineHeightPositions += CalculateReachableNineHeightPositions(map,y,x);
+            if(map[y][x] == 0){
+                (int uniqueReachedNineHeightPositions, int rating) results = CalculateReachableNineHeightPositionsAndRatings(map,y,x);
+                amountReachableNineHeightPositions += results.uniqueReachedNineHeightPositions;
+                sumRating += results.rating;
+            }
         }        
     }
 
-    return amountReachableNineHeightPositions;
+    return (amountReachableNineHeightPositions, sumRating);
 }
 
-int Task2(){
-    return 0;
-}
+(int,int) CalculateReachableNineHeightPositionsAndRatings(List<List<int>> map, int startY, int startX){
 
-int CalculateReachableNineHeightPositions(List<List<int>> map, int startY, int startX){
-    HashSet<(int,int)> uniqueReachedNineHeightPositions = new();
-
+    Dictionary<string, int> uniqueReachedNineHeightPositions = new();
+    
     WalkTrail(map,startY, startX, uniqueReachedNineHeightPositions);
+    int rating = uniqueReachedNineHeightPositions.Values.Sum();
 
-    return uniqueReachedNineHeightPositions.Count;
+    return (uniqueReachedNineHeightPositions.Count, rating);
 }
 
-void WalkTrail(List<List<int>> map, int y, int x, HashSet<(int,int)> uniqueReachedNineHeightPositions){
+void WalkTrail(List<List<int>> map, int y, int x, Dictionary<string, int> uniqueReachedNineHeightPositions){
     
     if(map[y][x] == 9){
-        uniqueReachedNineHeightPositions.Add((y,x));
+        string coordString = $"({y},{x})";
+        if(uniqueReachedNineHeightPositions.ContainsKey(coordString)){
+            uniqueReachedNineHeightPositions[coordString]++;
+        }
+        else{
+            uniqueReachedNineHeightPositions.Add(coordString,1);
+        }
         return;
     }
        
     var directions = DetermineWalkableDirections(map,y,x);
-    // foreach(var direction in directions){
-    //     Console.WriteLine($"({direction.Item1},{direction.Item2})");
-    // }
     foreach(var direction in directions){
         WalkTrail(map, y + direction.Item1, x + direction.Item2, uniqueReachedNineHeightPositions);
     }
@@ -73,7 +76,6 @@ List<(int,int)> DetermineWalkableDirections(List<List<int>> map,int y, int x){
     return acceptableDirections;
 }
 
-
 List<List<int>> ConvertToIntMap(List<string> inputLines){
     List<List<int>> map = new();
 
@@ -81,7 +83,7 @@ List<List<int>> ConvertToIntMap(List<string> inputLines){
         List<int> intline = new();
         foreach(char c in line){
             if(c == '.'){
-                intline.Add(0);
+                intline.Add(-1);
                 continue;
             }
             intline.Add(Convert.ToInt32(c + ""));
