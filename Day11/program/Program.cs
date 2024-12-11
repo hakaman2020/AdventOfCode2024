2f9b9293ab2ca3baca479e6b8b90f22b48d198cf
hakaman2020 <73297566+hakaman2020@users.noreply.github.com>
2024-12-11 09:58:29 +0100

    Day11 finished

--- Day11/program/Program.cs
+++ Day11/program/Program.cs
@@ -1,6 +1,6 @@
 ﻿
-string inputFilePath = "./example.txt";
-//string inputFilePath = "./input.txt";
+// string inputFilePath = "./example.txt";
+string inputFilePath = "./input.txt";
 
 List<string> fileLines = ReadFileLines(inputFilePath);
 List<long> stoneNumbers = fileLines[0].Split(' ').Select(n => long.Parse(n)).ToList();
@@ -9,24 +9,24 @@ List<long> stoneNumbers = fileLines[0].Split(' ').Select(n => long.Parse(n)).ToL
 //     Console.Write(stoneNumber + " ");
 // }
 // Console.WriteLine();
+int amountOfBlinksPart1 = 75;
+int amountOfBlinksPart2 = 6;
+Console.WriteLine($"Result of Task 1 is {GeneralTask(stoneNumbers, amountOfBlinksPart1)}");
+// Console.WriteLine($"Result of Task 2 is {GeneralTask(stoneNumbers, amountOfBlinksPart2 )}");
 
-Console.WriteLine($"Result of Task 1 is {Task1(stoneNumbers)}");
-Console.WriteLine($"Result of Task 2 is {Task2()}");
-
-int Task1(List<long> stoneNumbers){
+long GeneralTask(List<long> stoneNumbers, int amountOfBlinks){
     long totalAmountOfStones = 0;
-    Console.WriteLine(nextBlink(125,1,1));
-    // foreach(long number in stoneNumbers){
+    Dictionary<(long,int),long> memory = new();
 
-    // }
-    return 0;
+    foreach(long number in stoneNumbers){
+         Console.WriteLine("Processing :" + number);
+        totalAmountOfStones += nextBlink(number, amountOfBlinks, memory);
+    }
+    // totalAmountOfStones += nextBlink(125,amountOfBlinks,memory);
+    return totalAmountOfStones;
 }
 
-int Task2(){
-    return 0;
-}
-
-long nextBlink(long stoneNumber, int blinksLeft, long totalAmountOfStones){
+long nextBlink(long stoneNumber, int blinksLeft,Dictionary<(long,int),long> memory){
     
     blinksLeft--;
     string stoneNumberString = stoneNumber.ToString();
@@ -38,21 +38,46 @@ long nextBlink(long stoneNumber, int blinksLeft, long totalAmountOfStones){
     else if(stoneNumberString.Length % 2 == 0){
         stoneNumber = long.Parse(stoneNumberString.Substring(0, stoneNumberString.Length / 2));
         stoneNumber2 = long.Parse(stoneNumberString.Substring(stoneNumberString.Length / 2));
-        totalAmountOfStones++;
     }
     else{
         stoneNumber *= 2024;
     }
 
-    if(blinksLeft == 0)
-        return totalAmountOfStones;
-        
+    if(blinksLeft == 0){
+        // Console.WriteLine(stoneNumber);
+        if(stoneNumber2 != -1){
+            // Console.WriteLine(stoneNumber2);
+            return 2;
+        }
+        return 1;
+    }
 
+    long totalAmountOfStones = 0;
 
-    return 0;
+    if(memory.ContainsKey((stoneNumber,blinksLeft))){
+        // Console.WriteLine("Memory used (" + stoneNumber + " " + blinksLeft + "):" + memory[(stoneNumber,blinksLeft)]);
+        totalAmountOfStones = memory[(stoneNumber,blinksLeft)];
+    }
+    else{
+        totalAmountOfStones = nextBlink(stoneNumber, blinksLeft, memory);
+        memory.Add((stoneNumber,blinksLeft), totalAmountOfStones);
+        // Console.WriteLine("Memory stored (" + stoneNumber + " " + blinksLeft + "):" + memory[(stoneNumber,blinksLeft)]);
+    }
+
+    if(stoneNumber2 != -1){
+        if(memory.ContainsKey((stoneNumber2,blinksLeft))){
+            totalAmountOfStones += memory[(stoneNumber2,blinksLeft)];
+            // Console.WriteLine("Memory used (" + stoneNumber2 + " " + blinksLeft + "):" + memory[(stoneNumber2,blinksLeft)]);   
+        }
+        else{
+            long result = nextBlink(stoneNumber2, blinksLeft, memory);    
+            totalAmountOfStones += result;
+            memory.Add((stoneNumber2, blinksLeft),result);
+            // Console.WriteLine("Memory stored (" + stoneNumber2 + " " + blinksLeft + "):" + memory[(stoneNumber2,blinksLeft)]);
+        }
+    }
+    return totalAmountOfStones;
 }
-
-
 
 List<string> ReadFileLines(string inputFile){
     return File.ReadLines(inputFile).ToList();
